ef8f1b842f0e5f794dc41e8da66dd4cb10dac51c
Fenil Patel <fenilxpatel2642@gmail.com>
2026-09-05 22:16:59 +0530

    Delete stride(@) directory

--- stride(@)/app/src/main/java/com/example/data/model/RoutePoint.kt
+++
@@ -1,8 +0,0 @@
-package com.example.data.model
-
-data class RoutePoint(
-    val latitude: Double,
-    val longitude: Double,
-    val altitude: Double = 0.0,
-    val timestamp: Long = System.currentTimeMillis()
-)
